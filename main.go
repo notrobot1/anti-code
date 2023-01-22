33ff5c8b7002a1569972bf2ca7249c04fda4d277
NotRobot <user17@tutanota.com>
2023-01-21 20:13:01 -0800

    Update main.go

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ import (
 	//"github.com/go-rod/rod/lib/input"
 	//"github.com/go-rod/rod/lib/proto"
 	"github.com/gorilla/mux"
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	
 	//"github.com/go-rod/rod/lib/launcher"
 )
 
@@ -27,8 +33,55 @@ type runStruct struct{
 }
 
 
+
+func httpRequest(targetUrl string, method string, data []byte, headers map[string]string) *http.Response {
+
+	request, error := http.NewRequest(method, targetUrl, bytes.NewBuffer(data))
+	for k, v := range headers {
+		request.Header.Set(k, v)
+
+	}
+
+	customTransport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: customTransport}
+	response, error := client.Do(request)
+	defer response.Body.Close()
+
+	if error != nil {
+		panic(error)
+	}
+
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println("response Status:", response.Status)
+	fmt.Println("response Body:", string(body))
+	return response
+}
+
+
+
+
+
+
+
 func run(data runStruct){
 
+
+headers := map[string]string{
+    "Host": "square.github.io",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+    "Accept": "image/avif,image/webp,*/*",
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Referer": "https://square.github.io/okhttp/3.x/okhttp/okhttp3/Request.html",
+    "Sec-Fetch-Dest": "image",
+    "Sec-Fetch-Mode": "no-cors",
+    "Sec-Fetch-Site": "same-origin",
+    "Te": "trailers",
+}
+  var data1 = []byte(nil)
+  httpRequest("https://square.github.io:443/favicon.ico", "GET", data1, headers)
+	
 	for i := 0; i < data.UsersMax; i++ {
 		fmt.Printf("%T\n", data.Scope)
 		fmt.Printf("%T\n", data.UsersMax)
